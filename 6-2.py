6cdad3e6b14e086ca0c0dddaaf97ee4a69c523fd
Mark-Aurel1us <djezkazgan@rambler.ru>
2024-05-07 16:29:23 +0300

    we

--- 6-2.py
+++ 6-2.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy
 
-data = numpy.genfromtxt("res.txt")
+data = numpy.genfromtxt("down.txt")
 
 nu = data[-2]
 T = 1/nu
@@ -12,12 +12,14 @@ x = list(map(lambda t: t*T, range(len(data)-2)))
 y = list(map(lambda t: data[t]*dU, range(len(data)-2)))
 
 
-fig, ax = plt.subplots()
-ax.grid(True)
-ax.plot(x,y)
-ax.set(xticks = numpy.arange(0,len(data)-2,0.1), yticks = numpy.arange(0,max(data),0.1), xlim = (0,(len(data)-2)*T), ylim = (0,dU*max(data)))
+#fig, ax = plt.subplots()
+plt.plot(x,y, label = "U(t)")
+#ax.set(xticks = numpy.arange(0,len(data)-2), yticks = numpy.arange(0,max(data),), xlim = (0,(len(data)-2)*T), ylim = (0,dU*max(data)))
 
-
+#plt.yticks(ticks = numpy.arange(0,max(y),0.1), minor=True)
+#plt.xticks(numpy.arange(0,len(data)-2,0.1),minor=True)
+plt.tick_params(which="minor",alpha=0.3)
+plt.grid(which="minor",alpha=0.3)
 #plt.figure()
 #plt.title("")
 plt.xlabel("t, seconds")
